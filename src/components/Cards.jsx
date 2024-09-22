8655bd256568f81f0a6295e86e6d50713f5a58f8
QuantumRoshan <customuid10@gmail.com>
2024-09-21 18:45:14 -0600

    added job listing

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
 
-const Cards = ({ children, bg = 'red' }) => {
+const Cards = ({ children, bg = 'indigo' }) => {
   //red is the default if any colour is passed then it overwrites
   return (
     <div className={`bg-${bg}-100 p-6 rounded-lg shadow-md`}>{children}</div>
--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -6,7 +6,7 @@ const HomeCards = () => {
       <div className="container-xl lg:container m-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
           <div className="bg-gray-100 p-6 rounded-lg shadow-md">
-            <Cards>
+            <Cards bg="red">
               <h2 className="text-2xl font-bold">For Developers</h2>
               <p className="mt-2 mb-4">
                 Browse our React jobs and start your career today
