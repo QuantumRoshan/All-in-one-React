829228cf56a4bfdf934406465b31d301ee8b0e04
QuantumRoshan <customuid10@gmail.com>
2024-10-11 13:49:05 -0600

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import HomeCards from './components/HomeCards'
-import JobsListing from './components/JobsListing'
-import ViewAllJobs from './components/ViewAllJobs'
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/about" element={<h1>My App</h1>} />)
+)
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero title="React jobs" subtitle="Become a react dev" />
-      <HomeCards />
-      <JobsListing />
-      <ViewAllJobs />
-    </>
-  )
+  return <RouterProvider router={router} />
 }
-// new compenent added
 
 export default App
+
+// import Navbar from './components/Navbar'
+// import Hero from './components/Hero'
+// import HomeCards from './components/HomeCards'
+// import JobsListing from './components/JobsListing'
+// import ViewAllJobs from './components/ViewAllJobs'
+
+{
+  /* <>
+<Navbar />
+<Hero title="React jobs" subtitle="Become a react dev" />
+<HomeCards />
+<JobsListing />
+<ViewAllJobs />
+</> */
+}
